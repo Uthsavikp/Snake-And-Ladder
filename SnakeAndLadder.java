fd2a6bea0cec43cddd763db0261220f65e11c58e
uthsa <uthsavi29@gmail.com>
2020-12-14 23:21:57 +0530

    Added:To Get Random Die Roll Between 1 And 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,11 @@ public class SnakeAndLadder {
     int position=0;
     public static void main(String[] args){
         System.out.println("Welcome To Snake And Ladder Game");
+        System.out.println("Die Roll Is : " + getRandomDie(6,1));
+
+    }
+    public static int getRandomDie(int maximum, int minimum){
+        return ((int) (Math.random()*(maximum - minimum))) + minimum;
     }
 
 }
