6f2c5eeef3a57680bd715e3845ff7f0538aff889
uthsa <uthsavi29@gmail.com>
2020-12-14 21:35:43 +0530

    Modified:To Initialise Position Of Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,5 +1,6 @@
 public class SnakeAndLadder {
-    //main method
+    //variables
+    int position=0;
     public static void main(String[] args){
         System.out.println("Welcome To Snake And Ladder Game");
     }
