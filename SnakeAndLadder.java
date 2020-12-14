cfc7a0ce24bbae301e74f40ebde55ba2f83bc485
uthsa <uthsavi29@gmail.com>
2020-12-15 01:15:13 +0530

    Refactor:To Access Random Die Roll In Switch Case

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,13 +1,19 @@
+import java.util.Random;
 public class SnakeAndLadder {
     //variables
-    int position=0;
-    public static void main(String[] args){
+    int position = 0;
+
+    public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
-        System.out.println("Die Roll Is : " + getRandomDie(6,1));
-
+        SnakeAndLadder snakeLadder = new SnakeAndLadder();
+        snakeLadder.rollDice();
     }
-    public static int getRandomDie(int maximum, int minimum){
-        return ((int) (Math.random()*(maximum - minimum))) + minimum;
+    public int rollDice() {
+        Random random = new Random();
+        int dice = (random.nextInt(6)+1);
+        System.out.println("Die Roll Is : " + dice);
+        return dice;
+        }
     }
 
-}
+
