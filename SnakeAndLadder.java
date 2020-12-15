13024754decd77042bcbcb9366624235e7c305eb
uthsa <uthsavi29@gmail.com>
2020-12-15 17:51:44 +0530

    Added:To Repeat Till The Player Reaches Winning Position Hundred

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,16 @@
 import java.util.Random;
 public class SnakeAndLadder {
+    //constants
+    final static int WIN_POSITION = 100;
     //variables
     int position = 0;
+    int winTime = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
         SnakeAndLadder snakeLadder = new SnakeAndLadder();
         snakeLadder.playerOption();
+        snakeLadder.winningPosition();
     }
 
     public int rollDice() {
@@ -21,26 +25,29 @@ public class SnakeAndLadder {
         int dice = rollDice();
         int option = random.nextInt(3);
         System.out.println("Player Option Is : " + option);
-        System.out.println("0.No Play 1.Ladder 2.Snake");
         switch (option) {
 
-            case 0:
-                System.out.println("No Play");
-                break;
             case 1:
                 System.out.println("Ladder");
-                position +=dice;
+                position += dice;
                 break;
             case 2:
                 System.out.println("Snake");
-                position -=dice;
+                position -= dice;
                 break;
             default:
-                System.out.println("Error");
+                System.out.println("No Play");
                 break;
         }
     }
-}
 
+    public void winningPosition() {
+        while (position < WIN_POSITION) {
+            winTime += position;
+            position++;
+        }
+        System.out.println("Winning Position :" + position);
+    }
+}
 
 
